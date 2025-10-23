1ca083b272a0b70ea23f2c023e4ae86b81419bb2
AyushHarinkhede <ayushharinkhere2005@gmail.com>
2025-10-24 00:53:20 +0530

    division

--- CWC Sample.cpp
+++ CWC Sample.cpp
@@ -16,8 +16,7 @@ int main() {
 
     cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
 
-    return 0;
-}
+// This is a sample C++ program demonstrating basic input/output and addition of two numbers.
 
 // This is a sample C++ program demonstrating basic input/output and addition of two numbers.
 // It uses the C++17 language standard version.
@@ -34,4 +33,12 @@ cout << "The difference of " << num1 << " and " << num2 << " is " << difference 
 double product = num1 * num2;
 cout << "The product of " << num1 << " and " << num2 << " is " << product << endl;
 
-  
+  // division of two numbers
+if (num2 != 0) {
+    double quotient = num1 / num2;
+    cout << "The quotient of " << num1 << " and " << num2 << " is " << quotient << endl;
+} else {
+    cout << "Division by zero is not allowed." << endl;
+}
+return 0;
+}
